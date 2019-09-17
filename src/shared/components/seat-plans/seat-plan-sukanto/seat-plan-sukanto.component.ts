86efce8916421e8857d7d14ca306c770e6296243
Nazmul Alam <nazmuldipu@gmail.com>
2019-09-17 12:00:59 +0600

    sukanto babu seat plan updated

--- src/shared/components/seat-plans/seat-plan-sukanto/seat-plan-sukanto.component.ts
+++ src/shared/components/seat-plans/seat-plan-sukanto/seat-plan-sukanto.component.ts
@@ -1,106 +1,118 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, EventEmitter, Output } from '@angular/core';
-import { Category } from 'src/shared/models/category.model';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Seat } from 'src/shared/models/seat.model';
-import { User } from 'src/shared/models/user.model';
-import { SubBooking, Booking, SeatStatus } from 'src/shared/models/booking.model';
 
 @Component({
   selector: 'seat-plan-sukanto',
   templateUrl: './seat-plan-sukanto.component.html',
   styleUrls: ['./seat-plan-sukanto.component.scss']
 })
-export class SeatPlanSukantoComponent implements OnChanges {
-  @Input() seatList: Seat[];
-  @Input() date: any;
+export class SeatPlanSukantoComponent {
+  @Input() categoryName: string;
+  @Input() filteredSeatList: Seat[];
+  @Input() selectedSeat: Seat[];
 
-  @Output() booking = new EventEmitter<Booking>();
-
-  mode = 'SEAT_SOLD';
-
-  category: Category;
-  categoryList: Category[];
-
-  selectedSeat: Seat[];
-  filteredSeatList: Seat[];
+  @Output() select = new EventEmitter<number>();
 
   constructor() { }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.seatList && this.seatList.length > 0) {
-      this.createCategoryList();
-      this.selectedSeat = [];
-    }
+  onSeatClick(seatId) {
+    this.select.emit(seatId);
   }
 
-  createCategoryList() {
-    this.categoryList = [];
-
-    this.seatList.forEach(s => {
-      const cat: Category = s.category;
-      const c = this.categoryList.find(ct => ct.id == cat.id);
-      if (!c) {
-        this.categoryList.push(cat);
-      }
-    });
-    this.category = this.categoryList[0];
-    this.onSelectCategory(this.category.id);
-  }
-
-  onSelectCategory(categoryId: number) {
-    console.log(categoryId);
-    this.category = this.categoryList.find(ca => ca.id == categoryId);
-    console.log(this.category);
-    this.filterSeatList(categoryId);
-  }
-
-  filterSeatList(categoryId) {
-    this.filteredSeatList = [];
-    this.seatList.forEach(seat => {
-      if (seat.category.id == categoryId) {
-        this.filteredSeatList.push(seat);
-      }
-    });
-  }
-
-  selectedSeatContainSeatId(seatId): boolean {
+  selectedSeatContainSeatId(seatId) {
     return this.selectedSeat.some(e => e.id == seatId);
   }
+  // @Input() seatList: Seat[];
+  // @Input() date: any;
 
-  onSeatClick(seatId) {
-    if (this.selectedSeat.some(e => e.id === seatId)) {
-      const i = this.selectedSeat.findIndex(s => s.id == seatId);
-      this.selectedSeat.splice(i, 1);
-    } else {
-      const seat = this.filteredSeatList.find(se => se.id == seatId);
-      if (seat.available) {
-        this.selectedSeat.push(seat);
-      }
-    }
-  }
+  // @Output() booking = new EventEmitter<Booking>();
 
-  onCreateUser(event) {
-    const user: User = new User(event.name, event.phone);
-    const subbookingList: SubBooking[] = this.getSubbookingList(
-      this.selectedSeat
-    );
-    const booking: Booking = new Booking(user, subbookingList);
-    booking.eStatus = this.mode as SeatStatus;
+  // mode = 'SEAT_SOLD';
 
-    this.booking.emit(booking);
-    this.selectedSeat = [];
-  }
+  // category: Category;
+  // categoryList: Category[];
 
-  getSubbookingList(seatList: Seat[]): SubBooking[] {
-    let subBookingList: SubBooking[] = [];
-    seatList.forEach(seat => {
-      let sb: SubBooking = new SubBooking(this.makeDateString(this.date), seat);
-      subBookingList.push(sb);
-    });
-    return subBookingList;
-  }
+  // selectedSeat: Seat[];
+  // filteredSeatList: Seat[];
 
-  makeDateString(date) {
-    const dateString = date.year + '-' + date.month + '-' + date.day;
-    return dateString;
-  }
+  // constructor() { }
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   if (changes.seatList && this.seatList.length > 0) {
+  //     this.createCategoryList();
+  //     this.selectedSeat = [];
+  //   }
+  // }
+
+  // createCategoryList() {
+  //   this.categoryList = [];
+
+  //   this.seatList.forEach(s => {
+  //     const cat: Category = s.category;
+  //     const c = this.categoryList.find(ct => ct.id == cat.id);
+  //     if (!c) {
+  //       this.categoryList.push(cat);
+  //     }
+  //   });
+  //   this.category = this.categoryList[0];
+  //   this.onSelectCategory(this.category.id);
+  // }
+
+  // onSelectCategory(categoryId: number) {
+  //   console.log(categoryId);
+  //   this.category = this.categoryList.find(ca => ca.id == categoryId);
+  //   console.log(this.category);
+  //   this.filterSeatList(categoryId);
+  // }
+
+  // filterSeatList(categoryId) {
+  //   this.filteredSeatList = [];
+  //   this.seatList.forEach(seat => {
+  //     if (seat.category.id == categoryId) {
+  //       this.filteredSeatList.push(seat);
+  //     }
+  //   });
+  // }
+
+  // selectedSeatContainSeatId(seatId): boolean {
+  //   return this.selectedSeat.some(e => e.id == seatId);
+  // }
+
+  // onSeatClick(seatId) {
+  //   if (this.selectedSeat.some(e => e.id === seatId)) {
+  //     const i = this.selectedSeat.findIndex(s => s.id == seatId);
+  //     this.selectedSeat.splice(i, 1);
+  //   } else {
+  //     const seat = this.filteredSeatList.find(se => se.id == seatId);
+  //     if (seat.available) {
+  //       this.selectedSeat.push(seat);
+  //     }
+  //   }
+  // }
+
+  // onCreateUser(event) {
+  //   const user: User = new User(event.name, event.phone);
+  //   const subbookingList: SubBooking[] = this.getSubbookingList(
+  //     this.selectedSeat
+  //   );
+  //   const booking: Booking = new Booking(user, subbookingList);
+  //   booking.eStatus = this.mode as SeatStatus;
+
+  //   this.booking.emit(booking);
+  //   this.selectedSeat = [];
+  // }
+
+  // getSubbookingList(seatList: Seat[]): SubBooking[] {
+  //   let subBookingList: SubBooking[] = [];
+  //   seatList.forEach(seat => {
+  //     let sb: SubBooking = new SubBooking(this.makeDateString(this.date), seat);
+  //     subBookingList.push(sb);
+  //   });
+  //   return subBookingList;
+  // }
+
+  // makeDateString(date) {
+  //   const dateString = date.year + '-' + date.month + '-' + date.day;
+  //   return dateString;
+  // }
 }
