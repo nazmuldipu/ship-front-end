e62f4cfe217385e60d7fcab237a5447802c82541
Nazmul Alam <nazmuldipu@gmail.com>
2019-12-18 18:56:09 +0600

    admin print ticket done

---
+++ src/dashboard/admin/sell/containers/print-ticket/print-ticket.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { BookingService } from 'src/service/booking.service';
+import { Booking } from 'src/shared/models/booking.model';
+
+@Component({
+  selector: 'app-print-ticket',
+  templateUrl: './print-ticket.component.html',
+  styleUrls: ['./print-ticket.component.scss']
+})
+export class PrintTicketComponent {
+  ticketId;
+  ticket: Booking;
+  errorMessage = '';
+  constructor(private bookingService: BookingService) { }
+
+  getTicket() {
+    this.getAdminBooking(this.ticketId);
+  }
+
+  async getAdminBooking(bookingId) {
+    this.errorMessage = '';
+    this.ticket = null;
+    await this.bookingService.getAdminBooking(bookingId).subscribe(
+      data => {
+        if (data.id) {
+          if (data.cancelled == true) {
+            this.ticket = data;
+          } else {
+            this.errorMessage = 'This ticket has been canceled';
+          }
+        } else {
+          this.errorMessage = 'No ticket found with id ' + bookingId;
+        }
+      },
+      error => {
+        this.errorMessage = 'Invalid ticket id';
+      }
+    );
+  }
+
+  onTicketClose() {
+    this.ticket = null;
+  }
+
+  onCloseButton() {
+    this.ticket = null;
+    this.errorMessage = '';
+  }
+
+}
